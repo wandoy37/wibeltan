b06941a2d29160cc46abaaa611f6260db17d4a26
wandoy37 <riswandi035@gmail.com>
2024-05-25 09:18:44 +0800

    send notifikasi penambahan pemohon dan verified pemohon

--- app/Http/Controllers/PemohonController.php
+++ app/Http/Controllers/PemohonController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Materi;
 use App\Models\Pemohon;
+use App\Services\FonTelService;
+use App\Services\FonteService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 
 class PemohonController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      */
@@ -65,6 +68,9 @@ class PemohonController extends Controller
                 'verifikasi' => 'menunggu persetujuan'
             ]);
             $pemohon->materis()->attach($request->materis);
+
+            // Whatsapp Send
+            $this->sendWhatsAppNotification($pemohon);
 
             return redirect()->route('pemohon.index')->with('success', 'Permohonan Berhasil Di Buat');
         } catch (\Throwable $th) {
@@ -134,6 +140,11 @@ class PemohonController extends Controller
             ]);
             $pemohon->materis()->sync($request->materis);
 
+            if ($request->verifikasi == 'disetujui') {
+                // Whatsapp Send
+                $this->sendWhatsAppVerified($pemohon);
+            }
+
             return redirect()->route('pemohon.index')->with('success', 'Permohonan Berhasil Di Update');
         } catch (\Throwable $th) {
             DB::rollBack();
@@ -162,5 +173,71 @@ class PemohonController extends Controller
             DB::commit();
             return redirect()->back();
         }
+    }
+
+    private function sendWhatsAppNotification($pemohon)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://api.fonnte.com/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => [
+                'target' => '082148722747',
+                'message' => 'Hi, Operator *WIBELTAN* Ada Permohonan Wisata Belajar Pertanian Atas Nama ' . $pemohon->nama . ' Dari ' . $pemohon->asal . ', Segera Cek dan Lakukan Konfirmasi Pelaksanaan Melalui Link Berikut : ' . route('pemohon.edit', $pemohon->id),
+            ],
+            CURLOPT_HTTPHEADER => [
+                'Authorization: BRnQBvYJ@Z277QTqGokt'
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        if (curl_errno($curl)) {
+            $error_msg = curl_error($curl);
+            curl_close($curl);
+            throw new \Exception($error_msg);
+        }
+
+        curl_close($curl);
+        return $response;
+    }
+
+    private function sendWhatsAppVerified($pemohon)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://api.fonnte.com/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => [
+                'target' => $pemohon->no_hp,
+                'message' => 'Hi, ' . $pemohon->nama . ' Permohonan Wisata Belajar Pertanian Anda Sudah di Verifikasi dan akan di laksanakan pada tanggal ' . $pemohon->tanggal_pelaksanaan,
+            ],
+            CURLOPT_HTTPHEADER => [
+                'Authorization: BRnQBvYJ@Z277QTqGokt'
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        if (curl_errno($curl)) {
+            $error_msg = curl_error($curl);
+            curl_close($curl);
+            throw new \Exception($error_msg);
+        }
+
+        curl_close($curl);
+        return $response;
     }
 }
